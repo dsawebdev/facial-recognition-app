76bfb3727a496299a379550f9b64b5f17ce40403
dsawebdev <dvonanderson@gmail.com>
2018-11-28 16:30:36 -0500

    working on a few css changes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,12 +4,20 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return (
     <div>
+    <p className="f2">
+        {`Instructions:`}
+      </p> 
       <p className="f3">
-        {'This link will detect faces in your pictures. Give it a try'}
+        {`Copy the image address from any link on the web and paste it into the field below `}
       </p> 
       <div className='center'>
         <div className="pa4 br3 shadow-5 form center">
-          <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/>
+          <input 
+            type="text" 
+            className="f4 pa2 w-70 center" 
+            onChange={onInputChange}
+            placeholder="enter your image address here..."
+          />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-blue" onClick={onPictureSubmit}>Click to Analyze</button>
         </div>
       </div>    
